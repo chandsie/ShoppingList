1bc787cab957d5fc374a749ed0efa4362a1d4208
Shreyas Chand <shreyas.chand@gmail.com>
2012-10-25 16:55:41 -0700

    Refactored layout. again. Added networking code.

--- src/com/shreyaschand/czshopper/Home.java
+++ src/com/shreyaschand/czshopper/Home.java
@@ -1,10 +1,21 @@
 package com.shreyaschand.czshopper;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -23,7 +34,8 @@ public class Home extends Activity implements OnClickListener {
 
 	LinearLayout itemsListView;
 	LayoutInflater inflater;
-	PullToRefreshScrollView pullToRefreshView; 
+	PullToRefreshScrollView pullToRefreshView;
+	ConnectivityManager connMgr;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,27 +43,32 @@ public class Home extends Activity implements OnClickListener {
         setContentView(R.layout.activity_home);
         findViewById(R.id.add_button).setOnClickListener(this);
         itemsListView = (LinearLayout) findViewById(R.id.list);
+        
         inflater = (LayoutInflater)getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         
         pullToRefreshView = (PullToRefreshScrollView) findViewById(R.id.list_pull__scroller);
         pullToRefreshView.setOnRefreshListener(new OnRefreshListener<ScrollView>() {
             public void onRefresh(PullToRefreshBase<ScrollView> refreshView) {
-                new GetDataTask().execute();
+                new GetDataTask().execute(true);
             }
-        });     
+        });
+        
+        new GetDataTask().execute(false);
     }
 
-    private class GetDataTask extends AsyncTask<Void, Void, HashMap<String, ArrayList<String>>> {
+    private class GetDataTask extends AsyncTask<Boolean, Void, HashMap<String, ArrayList<String>>> {
         
         @Override
         protected void onPostExecute(HashMap<String, ArrayList<String>> result) {
             // Call onRefreshComplete when the list has been refreshed.
             pullToRefreshView.onRefreshComplete();
             super.onPostExecute(result);
+            
+            
             for(Map.Entry<String, ArrayList<String>> entry : result.entrySet()){
             	String category = entry.getKey();
             	ArrayList<String> items = entry.getValue();
-            	
             	LinearLayout listItem = (LinearLayout) inflater.inflate(R.layout.list_item, null);
         		((TextView)listItem.getChildAt(1)).setText(category);
         		listItem.getChildAt(0).setVisibility(View.GONE);
@@ -67,7 +84,28 @@ public class Home extends Activity implements OnClickListener {
         }
 
 		@Override
-		protected HashMap<String, ArrayList<String>> doInBackground(Void... params) {
+		protected HashMap<String, ArrayList<String>> doInBackground(Boolean... networkRefresh) {
+			    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+			    JSONArray jsonData = null;
+			    if (networkRefresh[0] && networkInfo != null && networkInfo.isConnected()) {
+			        // Fetch updates from server if asked to do so and if network is available
+			    	try {
+				    	URLConnection conn = new URL("http://czshopper.herokuapp.com/items.json").openConnection();
+						conn.setRequestProperty("Accept", "application/json");
+						conn.setRequestProperty("X-CZ-Authorization", "quqSxtRqyBowMcz46qKr");
+				    	conn.connect();
+						BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream())); 
+						String response = br.readLine();
+						br.close();
+						jsonData = new JSONArray(response);
+			    	} catch (IOException ioe){
+			    		// Network Error. Deal with it.
+			    	} catch (JSONException e) {
+						// Bad stuff happened. Probably not something I did.
+					}
+			    } else {
+			        // Use saved values from db
+			    }
 			HashMap<String, ArrayList<String>> result = new HashMap<String, ArrayList<String>>();
 			ArrayList<String> items = new ArrayList<String>();
 			items.add("Cherry Coke");
